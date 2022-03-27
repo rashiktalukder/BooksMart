8843c711b737993c987d782108100c8165487601
Kawsar Rashik <kawsar.rashik@bizzntek.com>
2022-03-28 03:02:51 +0600

    First Commit..Data Connection and CRUD Done..

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,127 @@
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db=db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<CategoryModel> objCategoryList = _db.categoryTable;
+            return View(objCategoryList);
+        }
+
+        //Get
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(CategoryModel categoryObj)
+        {
+            /*if (categoryObj.Name == null && categoryObj.DisplayOrder.ToString()==null)
+            {
+                ModelState.AddModelError("CustomError", "Name and Display Order is required!!");
+            }*/
+
+            if (categoryObj.Name == categoryObj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "Name & Display order can not be same");
+                // Or, ModelState.AddModelError("Name", "Name & Display order can not be same");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.categoryTable.Add(categoryObj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            return View(categoryObj);
+
+        }
+
+        //Get
+        public IActionResult Edit(int id)
+        {
+            if(id==null)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.categoryTable.Find(id);
+            //var categoryFromDbFirst = _db.categoryTable.FirstOrDefault(u=>u.Id==id);
+            //var categoryFromDbSingle = _db.categoryTable.SingleOrDefault(u => u.Id==id);
+            if(categoryFromDb==null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(CategoryModel categoryObj)
+        {
+            if (categoryObj.Name == categoryObj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "Name & Display order can not be same");
+                // Or, ModelState.AddModelError("Name", "Name & Display order can not be same");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.categoryTable.Update(categoryObj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+                return View(categoryObj);
+
+        }
+        //Get-> DeleteCategory
+        public IActionResult Delete(int id)
+        {
+            if (id==null)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.categoryTable.Find(id);
+            //var categoryFromDbFirst = _db.categoryTable.FirstOrDefault(u=>u.Id==id);
+            //var categoryFromDbSingle = _db.categoryTable.SingleOrDefault(u => u.Id==id);
+            if (categoryFromDb==null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int id)
+        {
+            var DeleteObj=_db.categoryTable.Find(id);
+            if(id==null)
+            {
+                return NotFound();
+            }
+            _db.categoryTable.Remove(DeleteObj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+
+            return View(DeleteObj);
+
+        }
+    }
+}
---
+++ BulkyBookWeb/Models/CategoryModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
+{
+    public class CategoryModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="This field is required")]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage ="Display order must be between 1 to 100 Only!!")]
+        public string DisplayOrder { get; set; }
+        //public DateTime? CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
